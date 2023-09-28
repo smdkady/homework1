2ae18ca9630feb503d72be0140e68ec8b871e784
vexed <kadirthekid@gmail.com>
2023-09-28 14:14:16 +0300

    changed src/

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 18620;
+        int x = 20;
+
+        System.out.println("Получен(о) " + (price / x) + " бонус(а/ов)");
+
+
+
+    }
+}
